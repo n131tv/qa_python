8db80e8bdc3b7a2e9c011b59536f958bce75e73e
Nikita Novikov <n.n.s84@mail.ru>
2025-06-02 19:56:05 +0300

    test

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from main import BooksCollector
 
+
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
